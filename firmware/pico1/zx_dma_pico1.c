f5b455384be1ad72d49b3dbb142f03fb73e3076b
Derek Fountain <derek@scratters.com>
2024-11-13 19:40:19 +0000

    2048 byte DMA working, takes approx 3mS which is too slow. top border time is 4ms

--- firmware/pico1/zx_dma_pico1.c
+++ firmware/pico1/zx_dma_pico1.c
@@ -94,7 +94,7 @@ void int_callback( uint gpio, uint32_t events )
   gpio_put( GPIO_P1_BLIPPER, 1 );
 
   uint32_t byte_counter;
-  for( byte_counter=0; byte_counter < 2; byte_counter++ )
+  for( byte_counter=0; byte_counter < 2048; byte_counter++ )
   {
     /*
      * Moving on to the right hand side of fig7 in the Z80 manual.
